9482d83988a6647eafef826fd11f4719d26655aa
Poison02 <2069820192@qq.com>
2024-03-07 23:04:16 +0800

    新增Chakra-ui的测试

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { RouterProvider } from 'react-router-dom'
 import { SWRConfig } from 'swr'
 
 import { router } from '@/routes'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import './index.css'
 
@@ -12,7 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <SWRConfig value={{ revalidateOnFocus: false }}>
       <Suspense fallback={<></>}>
-        <RouterProvider router={router} />
+        <ChakraProvider>
+          <RouterProvider router={router} />
+        </ChakraProvider>
       </Suspense>
     </SWRConfig>
   </React.StrictMode>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,14 @@
-import Login from "../login"
 import Navbar from "../navbar"
+import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 const Home: React.FC = () => {
 
   return (
     <>
       <Navbar />
+      <CircularProgress isIndeterminate color='green.300' />
+      <Button colorScheme='blue'>Button</Button>
     </>
   )
 }
